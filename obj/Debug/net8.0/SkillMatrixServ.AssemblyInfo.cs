37efe9969e200172f11cd887b0f856cce6598a33
amineadlouni <amine.adlouni29@gmail.com>
2025-10-31 16:27:01 +0100

    conflict detected

--- obj/Debug/net8.0/SkillMatrixServ.AssemblyInfo.cs
+++ obj/Debug/net8.0/SkillMatrixServ.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SkillMatrixServ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9000acdf94b86b19d2e5324650999f47858a6b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("SkillMatrixServ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SkillMatrixServ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
